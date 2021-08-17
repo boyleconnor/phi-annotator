5df4a378cba21478e7ded2c89b5771a9f33a036f
Connor Boyle <connor.bo@gmail.com>
2021-08-17 08:38:03 -0700

    Clean UP

--- server/openapi_server/controllers/text_location_annotation_controller.py
+++ server/openapi_server/controllers/text_location_annotation_controller.py
@@ -1,31 +1,9 @@
 import connexion
-import pandas as pd
 from openapi_server.annotator.phi_types import PhiType
 from openapi_server.get_annotations import get_annotations
 from openapi_server.models.error import Error  # noqa: E501
 from openapi_server.models.text_location_annotation_request import TextLocationAnnotationRequest  # noqa: E501
 from openapi_server.models.text_location_annotation_response import TextLocationAnnotationResponse  # noqa: E501
-
-
-class Data:
-    def __init__(self):
-        df = pd.read_csv("data/streets.csv")
-        self._streets = df['Street'].str.lower().unique().tolist()
-
-        df = pd.read_csv("data/cities.csv")
-        self._cities = df['City'].str.lower().unique().tolist()
-
-        df = pd.read_csv("data/states.csv")
-        self._states = df['State'].str.lower().unique().tolist()
-
-        df = pd.read_csv("data/countries.csv")
-        self._countries = df['Name'].str.lower().unique().tolist()
-
-        df = pd.read_csv("data/other_locations.csv")
-        self._others = df['Other'].str.lower().unique().tolist()
-
-
-data = Data()
 
 
 def create_text_location_annotations():  # noqa: E501
@@ -53,19 +31,3 @@ def create_text_location_annotations():  # noqa: E501
             status = 500
             res = Error("Internal error", status, str(error))
     return res, status
-
-
-def add_annotations(annotations, matches, location_type):
-    """
-    Converts matches to TextLocationAnnotation objects and adds them
-    to the annotations array specified.
-    """
-    for match in matches:
-        annotations.append(
-            TextLocationAnnotation(
-                start=match.start(),
-                length=len(match[0]),
-                text=match[0],
-                location_type=location_type,
-                confidence=95.5
-            ))
